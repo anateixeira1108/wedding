762c3cda8c666a2d65904433e1f527b798b7e6ef
Ana Teixeira Andrade <anateixeira1108@gmail.com>
2019-01-17 11:12:32 -0500

    Registry section and Back to the top added

--- js/main.js
+++ js/main.js
@@ -3,6 +3,22 @@
 
 	// Initiate the wowjs animation library
 	new WOW().init();
+	
+
+	// Back to top button
+	$(window).scroll(function() {
+		if ($(this).scrollTop() > 100) {
+	    	$('.back-to-top').fadeIn('slow');
+	  	} else {
+	    	$('.back-to-top').fadeOut('slow');
+	  	}
+	});
+
+	$('.back-to-top').click(function(){
+	  	$('html, body').animate({scrollTop : 0},1500, 'easeInOutExpo');
+	  	return false;
+	});
+
 
 })(jQuery);
 
